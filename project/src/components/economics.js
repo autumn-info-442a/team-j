e52dcc0a71d076f2005dfc1876c7174b6127695c
Chueh-Chen Lai <lai1001@uw.edu>
2020-12-15 17:58:29 +0800

    add reference

--- project/src/components/economics.js
+++ project/src/components/economics.js
@@ -20,6 +20,15 @@ function Economics() {
         <div id="word_cloud">
           <Chart />
         </div>
+        <div className="reference">
+                <p>References</p>
+                <ol>
+                    <li>“Data Lab – The Federal Response to COVID-19 – U.S. Treasury.” Data Lab – The Federal Response to COVID-19 – U.S. Treasury | Data Lab, www.datalab.usaspending.gov/federal-covid-funding/.</li>
+                    <li>“Coronavirus Bailouts: Which Country Has the Most Generous Deal?” BBC News, BBC, 7 May 2020, www.bbc.com/news/business-52450958.</li>
+                    <li>“all_jobs_sum_job_loss_county.Csv.” Urban Data Catalog, 4 Sept. 2020, www.datacatalog.urban.org/dataset/estimated-low-income-jobs-lost-covid-19/resource/5a29a102-bd87-49c2-a09d-6a09bc73a561. </li>
+                    <li>“industry_code_translation_list.” Urban Data Catalog, 15 Apr. 2020, www.datacatalog.urban.org/dataset/estimated-low-income-jobs-lost-covid-19/resource/cd4ef086-7401-4b63-9424-7881aa2be22d. </li>
+                </ol>
+            </div>
      </div>
     );
 }
--- project/src/components/health.js
+++ project/src/components/health.js
@@ -10,9 +10,17 @@ function Health() {
             <div id='timeline_slider'>
                 <DiscreteSlider />
             </div>
-            <div className="health_bottom">
+            <div>
                 <div><Mortality></Mortality></div>
                 <div><Condition></Condition></div>
+            </div>
+            <div className="reference">
+                <p>References</p>
+                <ol>
+                    <li>World Health Organization. What We Know About Long-term Effects of COVID-19. September 2020, https://www.who.int/docs/default-source/coronaviruse/risk-comms-updates/update-36-long-term-symptoms.pdf?sfvrsn=5d3789a6_2#:~:text=%E2%80%A2%20Most%20people%20with%20COVID,have%20lasting%20health%20effects. PowerPoint Presentation.</li>
+                    <li>“Provisional Death Counts for Coronavirus Disease (COVID-19): Distribution of Deaths by Race and Hispanic Origin.” Centers for Disease Control and Prevention, https://www.data.cdc.gov/NCHS/Provisional-Death-Counts-for-Coronavirus-Disease-C/pj7m-y5uh. </li>
+                    <li>“Symptom Duration and Risk Factors for Delayed Return to Usual Health Among Outpatients with COVID-19 in a Multistate Health Care Systems Network - United States, March–June 2020.” Centers for Disease Control and Prevention, Centers for Disease Control and Prevention, 30 July 2020, www.cdc.gov/mmwr/volumes/69/wr/mm6930e1.htm. </li>
+                </ol>
             </div>
       </div>
     );
