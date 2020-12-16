b92ab4d60ffa0279c5814ad2a3a92c2d7636c0c4
hahahedy <hedyj@uw.edu>
2020-12-15 19:56:16 -0800

    update nav

--- project/src/components/Nav.js
+++ project/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom'; 
 // import './App.css'; 
@@ -8,15 +8,24 @@ import { Link } from 'react-router-dom';
             color: 'black'
         }
 
+        const [isOpen, setIsOpen]=useState(false);
+        const [isOpen2, setIsOpen2]=useState(false);
+        const onClickHeader=()=> {
+            setIsOpen(!isOpen)
+        }
+        const onClickHeader2=()=> {
+            setIsOpen2(!isOpen2)
+        };
+
         return( 
             <nav className="nav_bar">
                 <img src="https://i.ibb.co/QdxMM5k/covid-logo.png" alt="covid_logo"/>
                 <ul className="nav_links">
                     <Link style={navStyle} to='/health'>
-                        <li>Health </li>
+                        <li onClick={onClickHeader} onClick={onClickHeader2} className={`${isOpen ? 'button' :''}`}>Health </li>
                     </Link>
                     <Link style={navStyle} to='/economics'>
-                        <li>Economics</li>
+                        <li onClick={onClickHeader2} onClick={onClickHeader} className={`${isOpen2 ? 'button' :''}`}>Economics</li>
                     </Link>
                 </ul>
             </nav>
